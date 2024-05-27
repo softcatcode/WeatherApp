1162aa1f76fb868589b39dfbdea3f788296f9db6
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-27 12:38:36 +0300

    Favourites Store/Factory was implemented.

--- app/src/main/java/com/softcat/weatherapp/di/components/ApplicationComponent.kt
+++ app/src/main/java/com/softcat/weatherapp/di/components/ApplicationComponent.kt
@@ -3,10 +3,11 @@ package com.softcat.weatherapp.di.components
 import android.content.Context
 import com.softcat.weatherapp.di.annotations.ApplicationScope
 import com.softcat.weatherapp.di.modules.DataModule
+import com.softcat.weatherapp.di.modules.PresentationModule
 import dagger.BindsInstance
 import dagger.Component
 @ApplicationScope
-@Component(modules = [DataModule::class])
+@Component(modules = [DataModule::class, PresentationModule::class])
 interface ApplicationComponent {
 
     @Component.Factory
