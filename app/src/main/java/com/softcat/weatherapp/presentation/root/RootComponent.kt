9e9ddff8cf3d2cf13822142d13760cc42db27b0d
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-27 15:26:29 +0300

    Root component was implemented.

--- app/src/main/java/com/softcat/weatherapp/presentation/root/RootComponent.kt
+++ app/src/main/java/com/softcat/weatherapp/presentation/root/RootComponent.kt
@@ -1,4 +1,20 @@
 package com.softcat.weatherapp.presentation.root
 
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
+import com.softcat.weatherapp.presentation.details.DetailsComponent
+import com.softcat.weatherapp.presentation.favourite.FavouritesComponent
+import com.softcat.weatherapp.presentation.search.SearchComponent
+
 interface RootComponent {
+
+    val stack: Value<ChildStack<*, Child>>
+    
+    sealed interface Child {
+        data class Favourites(val component: FavouritesComponent): Child
+
+        data class SearchCity(val component: SearchComponent): Child
+
+        data class CityDetails(val component: DetailsComponent): Child
+    }
 }
