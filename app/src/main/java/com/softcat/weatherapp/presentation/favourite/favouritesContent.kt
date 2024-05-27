48eda69537b0f3e58d0a6ea1233d83def596c775
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-27 15:39:14 +0300

    Navigation was implemented.

---
+++ app/src/main/java/com/softcat/weatherapp/presentation/favourite/favouritesContent.kt
@@ -0,0 +1,8 @@
+package com.softcat.weatherapp.presentation.favourite
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavouritesContent(component: FavouritesComponent) {
+
+}
---
+++ app/src/main/java/com/softcat/weatherapp/presentation/search/searchContent.kt
@@ -0,0 +1,8 @@
+package com.softcat.weatherapp.presentation.search
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchContent(component: SearchComponent) {
+
+}
