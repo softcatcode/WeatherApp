9111b81e264424d64b6e3cde6c4477461c12fb24
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-29 17:27:56 +0300

    Weather description is translated now on the details screen.

--- app/src/main/java/com/softcat/weatherapp/data/network/api/ApiFactory.kt
+++ app/src/main/java/com/softcat/weatherapp/data/network/api/ApiFactory.kt
@@ -6,16 +6,21 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import java.util.Locale
 
 object ApiFactory {
 
     private const val BASE_URL = "https://api.weatherapi.com/v1/"
     private const val KEY_PARAM = "key"
+    private const val LANGUAGE_PARAM = "lang"
 
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor { chain ->
             val originalReq = chain.request()
-            val newUrl = originalReq.url.newBuilder().addQueryParameter(KEY_PARAM, BuildConfig.WEATHER_API_KEY).build()
+            val newUrl = originalReq.url.newBuilder()
+                .addQueryParameter(KEY_PARAM, BuildConfig.WEATHER_API_KEY)
+                .addQueryParameter(LANGUAGE_PARAM, Locale.getDefault().language)
+                .build()
             val newReq = originalReq.newBuilder().url(newUrl).build()
             chain.proceed(newReq)
         }.addInterceptor(
