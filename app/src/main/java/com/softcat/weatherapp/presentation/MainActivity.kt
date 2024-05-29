8e95df088d00837c806c244ce0c09f4b4904d786
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-05-29 17:29:01 +0300

    MainActivity is cleaned.

--- app/src/main/java/com/softcat/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/softcat/weatherapp/presentation/MainActivity.kt
@@ -6,14 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.arkivanov.decompose.defaultComponentContext
 import com.softcat.weatherapp.WeatherApplication
-import com.softcat.weatherapp.domain.useCases.AddToFavouriteUseCase
-import com.softcat.weatherapp.domain.useCases.SearchCityUseCase
 import com.softcat.weatherapp.presentation.root.RootComponentImpl
 import com.softcat.weatherapp.presentation.root.RootContent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
@@ -21,19 +15,9 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var rootComponentFactory: RootComponentImpl.Factory
 
-    @Inject
-    lateinit var searchCityUseCase: SearchCityUseCase
-
-    @Inject
-    lateinit var addToFavouritesUseCase: AddToFavouriteUseCase
-
     override fun onCreate(savedInstanceState: Bundle?) {
         (applicationContext as WeatherApplication).component.inject(this)
         super.onCreate(savedInstanceState)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            searchCityUseCase("пон").forEach { addToFavouritesUseCase(it) }
-        }
 
         enableEdgeToEdge()
         setContent {
