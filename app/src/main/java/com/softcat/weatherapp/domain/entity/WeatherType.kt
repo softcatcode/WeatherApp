2380165dfa1c5ed91dcb11876c330b22e0155144
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-09-11 12:10:15 +0300

    Weather types' icons were added.

--- app/src/main/java/com/softcat/weatherapp/domain/entity/WeatherType.kt
+++ app/src/main/java/com/softcat/weatherapp/domain/entity/WeatherType.kt
@@ -1,9 +1,27 @@
 package com.softcat.weatherapp.domain.entity
 
+import com.softcat.weatherapp.R
+
 enum class WeatherType {
+    Thunderstorm,
+    Drizzle,
     Rain,
-    Snow,
+    ShowerRain,
+    Mist,
     Sun,
     Clouds,
-    Wind
+    HeavyClouds,
+    Snow
+}
+
+fun WeatherType.toIconResId() = when (this) {
+    WeatherType.Thunderstorm -> R.drawable.thunderstorm
+    WeatherType.Drizzle -> R.drawable.drizzle
+    WeatherType.Rain -> R.drawable.rain
+    WeatherType.ShowerRain -> R.drawable.shower
+    WeatherType.Mist -> R.drawable.mist
+    WeatherType.Sun -> R.drawable.sun
+    WeatherType.Clouds -> R.drawable.clouds
+    WeatherType.HeavyClouds -> R.drawable.heavy_clouds
+    WeatherType.Snow -> R.drawable.snow
 }
