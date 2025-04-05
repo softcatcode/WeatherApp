b35962ea979180ce4db7f1c6bd17074992f1fa16
Daniil <109597472+softcatcode@users.noreply.github.com>
2025-04-05 20:59:07 +0300

    Api service mock was added.

---
+++ app/src/test/java/com/softcat/weatherapp/objectMocks/ApiServiceMock.kt
@@ -0,0 +1,42 @@
+package com.softcat.weatherapp.objectMocks
+
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.softcat.weatherapp.data.network.api.ApiService
+import com.softcat.weatherapp.data.network.dto.CityDto
+import com.softcat.weatherapp.data.network.dto.WeatherCurrentDto
+import com.softcat.weatherapp.data.network.dto.WeatherForecastDto
+import com.softcat.weatherapp.jsonMocks.currentWeatherJson
+import com.softcat.weatherapp.jsonMocks.searchResultJson
+import com.softcat.weatherapp.jsonMocks.weatherForecastJson
+import com.softcat.weatherapp.jsonMocks.weatherHistoryJson
+import kotlinx.coroutines.runBlocking
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
+
+val apiServiceMock = mock(ApiService::class.java)
+
+private val gson = Gson()
+val currentWeatherDto = gson.fromJson(currentWeatherJson, WeatherCurrentDto::class.java)
+val weatherForecastDto = gson.fromJson(weatherForecastJson, WeatherForecastDto::class.java)
+val weatherHistoryDto = gson.fromJson(weatherHistoryJson, WeatherForecastDto::class.java)
+val searchResultDto = gson.fromJson(searchResultJson, JsonArray::class.java).map {
+    gson.fromJson(it, CityDto::class.java)
+}
+
+fun setupApiServiceMock() = runBlocking {
+    whenever(
+        apiServiceMock.loadCurrentWeather(anyString())
+    ).thenReturn(currentWeatherDto)
+    whenever(
+        apiServiceMock.loadForecast(anyString(), anyInt())
+    ).thenReturn(weatherForecastDto)
+    whenever(
+        apiServiceMock.loadWeatherHistory(anyString(), anyString(), anyString())
+    ).thenReturn(weatherHistoryDto)
+    whenever(
+        apiServiceMock.searchCity(anyString())
+    ).thenReturn(searchResultDto)
+}
