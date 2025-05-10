6f3f57fbaa6d8098103a3c19cfb70d906eff056f
Daniil <109597472+softcatcode@users.noreply.github.com>
2025-05-10 15:36:38 +0300

    Database module is created.

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.ksp)
     alias(libs.plugins.google.gms.google.services)
 }
 
 android {
-    namespace = "com.softcat.data"
+    namespace = "com.softcat.database"
     compileSdk = 36
 
     defaultConfig {
@@ -14,10 +13,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
-
-        val apiKey = property("weatherApiKey")?.toString() ?:
-            error("No weather api key defined in gradle.properties.")
-        buildConfigField("String", "WEATHER_API_KEY", "\"$apiKey\"")
     }
 
     buildTypes {
@@ -36,9 +31,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        buildConfig = true
-    }
 }
 
 dependencies {
@@ -46,31 +38,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.timber)
-    implementation(project(":domain"))
-    implementation(project(":database"))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(libs.room.core)
-    ksp(libs.room.compiler)
-
-    implementation(libs.dagger.core)
-    ksp(libs.dagger.compiler)
-
-    implementation(libs.retrofit.core)
-    implementation(libs.retrofit.gsonConverter)
-    implementation(libs.okhttp)
-
-    implementation(libs.play.services.location)
-    implementation(libs.datastore)
-
-    testImplementation(project(":data"))
-    androidTestImplementation(project(":data"))
-
-    testImplementation(libs.mockito)
-    androidTestImplementation(libs.mockito)
 }
