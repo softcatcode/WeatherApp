5ecc89dd9103a44e500bca6a2e0f1794709f7dec
Daniil <109597472+softcatcode@users.noreply.github.com>
2025-06-16 02:29:35 +0300

    Cleaning code.

--- app/src/androidTest/java/com/softcat/weatherapp/SearchCityTest.kt
+++ app/src/androidTest/java/com/softcat/weatherapp/SearchCityTest.kt
@@ -5,18 +5,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.softcat.database.model.CityDbModel
 import com.softcat.database.model.CountryDbModel
 import kotlinx.coroutines.runBlocking
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 import kotlin.math.abs
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class SearchCityTest {
 
